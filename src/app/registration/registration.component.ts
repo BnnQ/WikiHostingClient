600dc0025946998216fde152f977bce2591e460b
r4yz7 <mmischenko850@gmail.com>
2024-03-25 15:19:35 +0200

    add register and login

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+
+export class RegistrationComponent{
+
+  email:string="";
+  username:string="";
+  password:string="";
+  agreed:boolean = false
+  errorMsg:string = "";
+  
+  constructor(private http:HttpClient, private router: Router){
+  }
+
+  register(){
+    if(this.agreed){
+      const credentials = {
+        email:this.email,
+        username:this.username,
+        password: this.password
+      };
+
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      };
+
+      console.log(credentials);
+
+      this.http.post('https://localhost:7133/api/Users/register',credentials,httpOptions).subscribe(
+        (response)=>{
+          console.log('Registration successful',response);
+          this.router.navigate(['/signin']);
+        },
+        (error)=>{
+          console.error('Registration failed', error);
+          this.errorMsg = "You entered incorrect information!";
+        }
+      );
+    }
+    else{
+      alert('Please agree to the terms')
+    }
+  }
+
+}
