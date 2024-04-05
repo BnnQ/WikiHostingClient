403f31c1f79d3595db1b1cf02a04cb66614e078c
BnnQ <1v25fa94@gmail.com>
2024-04-05 20:44:23 +0200

    Added environments

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  serverUrl: 'https://localhost:7133',
+  serverApiUrl: 'https://localhost:7133'
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  serverUrl: 'https://localhost:7133',
+  serverApiUrl: 'https://localhost:7133'
+};
