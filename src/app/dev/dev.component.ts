250e04cf2c998da69de37daefd18f1ad22b1aa0f
BnnQ <1v25fa94@gmail.com>
2024-04-05 20:48:01 +0200

    Added dev component to test standalone components (such as modals or text editor)

---
+++ src/app/dev/dev.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dev',
+  templateUrl: './dev.component.html',
+  styleUrl: './dev.component.css'
+})
+export class DevComponent {
+
+}
