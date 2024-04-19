15172998049d0a8fe29dc3a554871c326e6533a4
BnnQ <1v25fa94@gmail.com>
2024-04-19 20:28:27 +0200

    Improved existing repositories with interfaces

--- src/services/abstractions/i-user-repository.ts
+++ src/services/abstractions/i-user-repository.ts
@@ -2,7 +2,7 @@ import {User} from "../../models/user";
 import {UserUpsertDto} from "../../models/dto/user-upsert-dto";
 
 export interface IUserRepository {
-    getUser(id: number): Promise<User>;
+    getUser(id?: number): Promise<User>;
     registerUser(model: UserUpsertDto): Promise<User>;
     authUser(id: number, model: UserUpsertDto): Promise<User>;
 }
--- src/services/abstractions/i-wiki-repository.ts
+++ src/services/abstractions/i-wiki-repository.ts
@@ -2,13 +2,16 @@ import {Wiki} from "../../models/wiki";
 import {WikiUpsertDto} from "../../models/dto/wiki-upsert-dto";
 import {Contributor} from "../../models/contributor";
 import {ContributorUpsertDto} from "../../models/dto/contributor-upsert-dto";
+import {ContributorRole} from "../../models/contributor-role";
 
 export interface IWikiRepository {
   getWikis(search? : string) : Promise<Wiki[]>;
   getWiki(id: number) : Promise<Wiki | undefined>;
+  createWiki(model: WikiUpsertDto) : Promise<Wiki>;
   updateWiki(id: number, model: WikiUpsertDto) : Promise<Wiki>;
   deleteWiki(id: number): Promise<void>;
 
+  getContributorRoles() : Promise<ContributorRole[]>;
   getWikiContributors(wikiId: number, role?: string) : Promise<Contributor[]>;
   getWikiContributor(wikiId: number, userId: number) : Promise<Contributor | undefined>;
   addWikiContributor(wikiId: number, userId: number, model: ContributorUpsertDto) : Promise<void>;
