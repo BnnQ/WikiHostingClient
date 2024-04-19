81b261bd8bf82610310cd3b847248ebb7c635e5f
BnnQ <1v25fa94@gmail.com>
2024-04-19 20:35:08 +0200

    Starting up the signal notification service in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {SERVICE_IDENTIFIERS} from "./app.module";
+import {INotificationService} from "../services/abstractions/i-notification-service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WikiHostingClient';
+  signalConnectionId : string = '';
+
+  constructor(@Inject(SERVICE_IDENTIFIERS.INotificationService) private readonly notificationService: INotificationService) {
+  }
+
+  async ngOnInit(): Promise<void> {
+        this.signalConnectionId = await this.notificationService.getSelfConnectionId();
+    }
 }
