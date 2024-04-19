207546bfae974ef390540b9c4099d67ace9af510
BnnQ <1v25fa94@gmail.com>
2024-04-19 20:30:53 +0200

    Added NotificationHubUrl to environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   serverUrl: 'https://localhost:7133',
-  serverApiUrl: 'https://localhost:7133'
+  serverApiUrl: 'https://localhost:7133',
+  notificationHubUrl: 'https://localhost:7133/notificationHub'
 };
